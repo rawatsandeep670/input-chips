f8f9df915ab982cad41fdc19fab862b9b7586647
unknown <rawatsandeep670@gmail.com>
2016-05-07 23:00:41 +0530

    Add Delimeters

--- input-chips.js
+++ input-chips.js
@@ -1,5 +1,19 @@
 (function(){
     var app = angular.module("inputChipsApp", []);
+    
+    // Controller Code
+    app.controller("myCon", ['$scope', function($scope){
+        $scope.chipsData = {
+            to : []
+        }
+        $scope.chipsBreakDelimiter  = [13, 188, 32];
+        
+        $scope.getChips = function(){
+            alert($scope.mailData.to);
+        }
+    }]);
+    
+    // Directive Code
     app.directive("inputChips", inputChipsFun);
     inputChipsFun.$inject = [];
 
@@ -8,43 +22,50 @@
             restrict : 'EA',
             scope : {
               chipsValue : "=",
-              chipsUnique : "="
+              chipsUnique : "=",
+              chipsBreak : "="
             },
-            template: '<div class="chips"><div class="chips-item"></div><input type="text" ng-model="chipsModel" ng-keydown="inputKeyDown($event, false)" ng-blur="inputKeyDown($event, true)"/></div>',
+            template: '<div class="chips"><div class="chips-item"></div><input type="text" ng-model="chipsModel" autofocus ng-keydown="inputKeyDown($event, false)" ng-blur="inputKeyDown($event, true)"/></div>',
             link : function(scope, elem, attr){
               scope.chipsModel = null;
               var tempChipsArr = [];
+              if(scope.chipsBreak == undefined){
+                 scope.chipsBreak = [13];
+              }
               scope.inputKeyDown = function($event, is_blur){
-                if($event.keyCode==13 || is_blur){
-                  if(scope.chipsModel!=null){
+                for(var i=0; i<scope.chipsBreak.length; i++){
+                    if($event.keyCode==scope.chipsBreak[i] || is_blur){
+                        if(scope.chipsModel!=null){
 
-                    if(scope.chipsUnique){
-                      if(tempChipsArr.indexOf(scope.chipsModel)!=-1){
-                        scope.chipsModel = null;
-                        return;
-                      }
+                          if(scope.chipsUnique){
+                            if(tempChipsArr.indexOf(scope.chipsModel)!=-1){
+                              scope.chipsModel = null;
+                              return;
+                            }
+                          }
+
+                          //******* Regular Expression for Email ID
+                          var patt = new RegExp("/*\@/*");
+                          if(patt.test(scope.chipsModel)==false){
+                            scope.chipsModel = null;
+                            return;
+                          }
+                          //*******End Regular Expression for Email ID
+
+                          elem.find(".chips-item").append("<span class='chips-animation' value='"+(angular.element(".close-chip").length)+"'>"+scope.chipsModel+"<span class='close-chip ion-android-cancel'></span></span>");
+                          tempChipsArr.push(scope.chipsModel);
+                          scope.chipsModel = null;
+
+                          angular.element(".close-chip").on("click", function(){
+                              $(this).parent().css("display", "none");
+                              tempChipsArr[$(this).parent().attr("value")] = -1;
+                              chipsArrayUpdate();
+                          });
+
+                          chipsArrayUpdate();
+                        }
+                        $event.preventDefault();
                     }
-
-                    //******* Regular Expression for Email ID
-                    var patt = new RegExp("/*\@/*");
-                    if(patt.test(scope.chipsModel)==false){
-                      scope.chipsModel = null;
-                      return;
-                    }
-                    //*******End Regular Expression for Email ID
-
-                    elem.find(".chips-item").append("<span class='chips-animation' value='"+(angular.element(".close-chip").length)+"'>"+scope.chipsModel+"<span class='close-chip ion-android-cancel'></span></span>");
-                    tempChipsArr.push(scope.chipsModel);
-                    scope.chipsModel = null;
-
-                    angular.element(".close-chip").on("click", function(){
-                        $(this).parent().css("display", "none");
-                        tempChipsArr[$(this).parent().attr("value")] = -1;
-                        chipsArrayUpdate();
-                    });
-
-                    chipsArrayUpdate();
-                  }
                 }
 
                 if($event.keyCode==8 && scope.chipsValue.length>0){
